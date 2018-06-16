8da3d3e2225cf62a3bf155c84e2479ca231b4bdf
David Hersey <hersey.da@husky.neu.edu>
2018-06-16 09:52:23 -0700

    create course grid and whiteboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HelloWorldComponent } from './hello-world/hello-world.component';
+import {FormsModule} from "@angular/forms";
+import {CourseGridComponent} from "./course-grid/course-grid.component";
+import {WhiteBoardComponent} from "./white-board/white-board.component";
+import {RouterModule} from "@angular/router";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HelloWorldComponent,
+    CourseGridComponent,
+    WhiteBoardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
